ef86cfef87d8bf8332d0e9a6405d2c7282a8c0d4
jax authors <google-ml-automation@google.com>
2024-09-17 22:10:12 -0700

    Rewrite mosaic concat to support operand shapes that do not align with native shapes, Expand tests to cover multi operand, batch dim concat, etc.
    PiperOrigin-RevId: 675835891

--- jaxlib/mosaic/dialect/tpu/tpu_ops.cc
+++ jaxlib/mosaic/dialect/tpu/tpu_ops.cc
@@ -659,6 +659,36 @@ LogicalResult ShuffledStoreOp::canonicalize(ShuffledStoreOp op,
   }
   return success();
 }
+
+LogicalResult ConcatenateOp::verify() {
+  auto dimension = getDimension();
+  if (getOperands().size() < 2) {
+    return emitOpError("Expected at least 2 operands for concatenate op.");
+  }
+  auto first_type = getOperand(0).getType().cast<VectorType>();
+  auto first_shape = first_type.getShape();
+  auto first_dtype = first_type.getElementType();
+  for (int i = 0; i < getNumOperands(); ++i) {
+    auto operand = getOperand(i);
+    auto vty = cast<VectorType>(operand.getType());
+    auto shape = vty.getShape();
+    auto dtype = vty.getElementType();
+    if (dtype != first_dtype) {
+      return emitOpError(
+          "Not implemented:: Expected all operands to have the same element "
+          "type.");
+    }
+    for (int dim = 0; dim < shape.size(); ++dim) {
+      if (dim != dimension && shape[dim] != first_shape[dim]) {
+        return emitOpError(
+            "Not implemented: Expected all operands to have "
+            "the same shape outside of the concat dim");
+      }
+    }
+  }
+  return success();
+}
+
 }  // namespace tpu
 }  // namespace mlir
 
